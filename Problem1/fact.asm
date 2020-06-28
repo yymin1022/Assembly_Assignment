86e1cb50bb0e060a8de1e002bf7419fa878967ee
yymin1022 <yymin1022@gmail.com>
2020-06-29 01:30:04 +0900

    Fix Comment Mistype

--- Problem1/fact.asm
+++ Problem1/fact.asm
@@ -4,9 +4,9 @@ INCLUDE Irvine32.inc
 .code
 
 main PROC
-		push 4               : Calculate 5 Factorial
-		call Factorial       : Calculate Factorial(eax)
-		call WriteDec       : Show Result
+		push 4               ; Calculate 5 Factorial
+		call Factorial       ; Calculate Factorial(eax)
+		call WriteDec       ; Show Result
 		call Crlf
 		exit
 
@@ -15,27 +15,27 @@ main ENDP
 Factorial PROC
 	push ebp
 	mov ebp, esp
-	mov eax, [ebp + 8]  : Get n
-	cmp eax, 0              : is n < 0 ?
- 	ja L1                       : true -> Continue
-	mov eax, 1              : false -> return 1
+	mov eax, [ebp + 8]  ; Get n
+	cmp eax, 0              ; is n < 0 ?
+ 	ja L1                       ; true -> Continue
+	mov eax, 1              ; false -> return 1
 	jmp L2
 
 L1:
 	dec eax
-	push eax                 : Factorial(n - 1)
+	push eax                 ; Factorial(n - 1)
 	call Factorial
 
 : Instructions from this point on execute when
 : Each Recursive Call returns.
 
 ReturnFact:
-	mov ebx, [ebp + 8]  : get n
-	mul ebx                   : ax = ax * bx
+	mov ebx, [ebp + 8]  ; get n
+	mul ebx                   ; ax = ax * bx
 
 L2:
-	pop ebp                  : return EAX
-	ret 4                        : Clean up Stack
+	pop ebp                  ; return EAX
+	ret 4                        ; Clean up Stack
 
 Factorial ENDP
 
