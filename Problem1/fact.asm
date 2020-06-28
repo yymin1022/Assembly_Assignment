79bc0488729bc1b822cde044d383c0e9a6d052f2
yymin1022 <yymin1022@gmail.com>
2020-06-28 23:11:03 +0900

    Removed Comments because of Compile Error

--- Problem1/fact.asm
+++ Problem1/fact.asm
@@ -4,9 +4,9 @@ INCLUDE Irvine32.inc
 .code
 
 main PROC
-		push 4               : Calculate 5 Factorial
-		call Factorial       : Calculate Factorial(eax)
-		call WriteDec       : Show Result
+		push 4
+		call Factorial
+		call WriteDec
 		call Crlf
 		exit
 
@@ -15,27 +15,24 @@ main ENDP
 Factorial PROC
 	push ebp
 	mov ebp, esp
-	mov eax, [ebp + 8]  : Get n
-	cmp eax, 0              : is n < 0 ?
- 	ja L1                       : true -> Continue
-	mov eax, 1              : false -> return 1
+	mov eax, [ebp + 8]
+	cmp eax, 0
+ 	ja L1
+	mov eax, 1
 	jmp L2
 
 L1:
 	dec eax
-	push eax                 : Factorial(n - 1)
+	push eax
 	call Factorial
 
-: Instructions from this point on execute when
-: Each Recursive Call returns.
-
 ReturnFact:
-	mov ebx, [ebp + 8]  : get n
-	mul ebx                   : ax = ax * bx
+	mov ebx, [ebp + 8]
+	mul ebx
 
 L2:
-	pop ebp                  : return EAX
-	ret 4                        : Clean up Stack
+	pop ebp
+	ret 4
 
 Factorial ENDP
 
